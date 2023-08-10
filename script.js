960dc3d1e64f2e284036fd07037e7efb1d041224
SaNetPro <141953319+sanetpro@users.noreply.github.com>
2023-08-10 21:38:20 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,16 @@
-const countdown = document.getElementById('timer');
-let timeLeft = 300; // 5 minutes in seconds
+document.addEventListener("DOMContentLoaded", function() {
+    const countdown = document.getElementById('timer');
+    let timeLeft = 300; // 5 minutes in seconds
 
-function updateTimer() {
-    const minutes = Math.floor(timeLeft / 60);
-    const seconds = timeLeft % 60;
-    countdown.textContent = `${String(minutes).padStart(2, '0')}:${String(seconds).padStart(2, '0')}`;
-    if (timeLeft > 0) {
-        timeLeft--;
-        setTimeout(updateTimer, 1000);
+    function updateTimer() {
+        const minutes = Math.floor(timeLeft / 60);
+        const seconds = timeLeft % 60;
+        countdown.textContent = `${String(minutes).padStart(2, '0')}:${String(seconds).padStart(2, '0')}`;
+        if (timeLeft > 0) {
+            timeLeft--;
+            setTimeout(updateTimer, 1000);
+        }
     }
-}
 
-updateTimer();
+    updateTimer();
+});
